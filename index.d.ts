32c4e822bd5e357e8d40e95c614a183a1fa67706
2689 <99571603@qq.com>
2019-04-01 16:56:30 +0800

    [ADD] release 0.1.0

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+export declare const icons: {
+  favorite: string[];
+  default: string[];
+};
+
+export declare const categories: {
+  whatsNew: string[];
+  direction: string[];
+  suggestion: string[];
+  edit: string[];
+  data: string[];
+  other: string[];
+  series: string[];
+};
